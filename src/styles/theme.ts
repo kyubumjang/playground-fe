73f02f52461a02cc28399186686d8cecc626390f
Kyubum Jang <jkb2221@gmail.com>
2022-08-21 21:17:58 +0900

    style: 글로벌 스타일 임시 구성

---
+++ src/styles/theme.ts
@@ -0,0 +1,36 @@
+import { Theme } from '@emotion/react';
+
+const theme: Theme = {
+  color: {
+    primary: '#F19A78',
+    primaryHover: '#d88a6c',
+    primaryActive: '#c07b60',
+    secondary: '#1FAB89',
+    secondaryHover: '#1b997b',
+    secondaryActive: '#18886d',
+    background: '#fff',
+    yellow: '#FDCF65',
+    gray700: '#212529',
+    gray600: '#343A40',
+    gray500: '#495057',
+    gray400: '#ADB5BD',
+    gray300: '#CED4DA',
+    gray200: '#E9ECEF',
+    gray100: '#F8F9FA',
+    green200: '#62D2A2',
+    green100: '#D7FBE8',
+  },
+  fontSize: {
+    h1: '24px',
+    h2: '22px',
+    b1: '20px',
+    b2: '18px',
+    b3: '16px',
+    b4: '14px',
+    c1: '12px',
+    c2: '11px',
+  },
+  borderRadius: '8px',
+};
+
+export default theme;
